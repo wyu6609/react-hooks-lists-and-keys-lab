47ab9577c8437d2d00bbc765d10e8d6d5516385e
William Yu <wy427@nyu.edu>
2022-02-20 10:59:20 -0500

    assignment completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,14 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  let linksElements = links.map((link) => {
+    return (
+      <a key={link} href={"#" + link}>
+        {link}
+      </a>
+    );
+  });
+  return <nav>{linksElements}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technologiesEl = technologies.map((technology) => {
+    return <span key={technology}>{technology}</span>;
+  });
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{technologiesEl}</div>
     </div>
   );
 }
